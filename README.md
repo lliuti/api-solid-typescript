01fe7db7257550d56f5c0408bfd5cb9dfd24d358
Lucas Liuti <dev.lliuti@gmail.com>
2021-09-13 00:46:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-typescript-solid-api
+ Node.JS API using Typescript to understand the concepts of S.O.L.I.D.
---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,33 @@
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
+
+export class MailtrapMailProvider implements IMailProvider {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "70bb989c3db90c",
+        pass: "2206f7fe3b8b16",
+      },
+    });
+  }
+
+  async sendMail(message: IMessage): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email,
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email,
+      },
+      subject: message.subject,
+      html: message.body,
+    });
+  }
+}
